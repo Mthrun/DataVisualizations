c2639925160a8bd814022c0a763105cd1d2c3542
Mthrun <m.thrun@gmx.net>
2018-06-25 16:25:17 +0200

    .

--- R/ClassViolinPlot.R
+++ R/ClassViolinPlot.R
@@ -79,7 +79,7 @@ ClassViolinPlot <- function(Data, Cls, ColorSequence = DataVisualizations::Defau
                      labels = ClassNames,
                      name = "Classes") +
     
-    ggtitle(main)
+    ggtitle(main)+ theme(axis.text.x = element_text(angle = 90, hjust = 1,size = rel(1.2)))
   
   if (!PlotLegend)
     ggobject <- ggobject + theme(legend.position = "none")
