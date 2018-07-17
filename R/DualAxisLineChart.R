761c542c0f9c0e76e8eec7901e760b2aa5fae121
Mthrun <m.thrun@gmx.net>
2018-07-17 12:53:57 +0200

    bugfix

--- R/DualAxisLineChart.R
+++ R/DualAxisLineChart.R
@@ -1,10 +1,11 @@
 DualAxisLineChart=function(X,Y1,Y2,xlab='X',y1lab='Y1',y2lab='Y2',main='Dual Axis Line Chart',cols=c('black','blue'),SaveIt=FALSE){
   requireNamespace('plotly')
+
   
-  p <- plotly::plot_ly() %>%
-    plotly::add_lines(x = ~X, y = ~Y1, name = y1lab, line = list(color=cols[1])) %>%
-    plotly::add_lines(x = ~X, y = ~Y2, name = y2lab, yaxis = "y2", line = list(color=cols[2])) %>%
-    plotly::layout(
+  p <- plotly::plot_ly() 
+    p <- plotly::add_lines(p,x = ~X, y = ~Y1, name = y1lab, line = list(color=cols[1])) 
+    p <-  plotly::add_lines(p,x = ~X, y = ~Y2, name = y2lab, yaxis = "y2", line = list(color=cols[2])) 
+    p <-  plotly::layout(p,
       title = main, 
       yaxis2 = list(
         overlaying = "y",
@@ -19,7 +20,7 @@ DualAxisLineChart=function(X,Y1,Y2,xlab='X',y1lab='Y1',y2lab='Y2',main='Dual Axi
         showgrid=FALSE
         )
     )
-
+	p
   if(SaveIt){
     requireNamespace('htmlwidgets')
     htmlwidgets::saveWidget(p, file = "DualAxisLineChart.html")
