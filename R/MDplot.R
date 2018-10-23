cc95b8e351e16f02e85d1ab91705b16aa53c575d
Mthrun <m.thrun@gmx.net>
2018-10-23 09:18:48 +0200

    mdplot improved

--- R/MDplot.R
+++ R/MDplot.R
@@ -1,4 +1,4 @@
-MDplot = PDEviolinPlot = function(Data, Names,fill='darkblue',scale='width',size=0.01){
+MDplot = PDEviolinPlot = function(Data, Names,fill='darkblue',StatsEval=FALSE,scale='width',size=0.01){
   #MDplot(data, Names)
   # Plots a Boxplot like pdfshape for each column of the given data
   #
@@ -44,10 +44,46 @@ MDplot = PDEviolinPlot = function(Data, Names,fill='darkblue',scale='width',size
   colnames(dataframe) <- c('ID', 'Variables', 'Values')
   dataframe$Variables=as.character(dataframe$Variables)
 
-    plot <-
+    plot =
       ggplot(data = dataframe,
              aes_string(x = "Variables", group = "Variables", y = "Values")) +
       geom_violin(stat = "PDEdensity",fill=fill,scale=scale,size=size)+ theme(axis.text.x = element_text(size=rel(1.2)))
-
+    
+    if(isTRUE(StatsEval)){
+      requireNamespace('moments')
+      requireNamespace('diptest')
+    x=as.matrix(Data)
+    lowInnerPercentile=25
+    hiInnerPercentile = 100 - lowInnerPercentile
+    faktor <- sum(abs(qnorm(t(c(lowInnerPercentile, hiInnerPercentile)/100), 0, 1)))
+    std <- sd(x, na.rm = TRUE)
+    p <- c(lowInnerPercentile, hiInnerPercentile)/100
+    quartile <- prctile(x, p)
+    if (ncol(x) > 1) 
+      iqr <- quartile[2, ] - quartile[1, ]
+    else iqr <- quartile[2] - quartile[1]
+    shat <- c()
+    mhat <- c()
+    nonunimodal=c()
+    skewed=c()
+    Nsample=10000
+    kernels=matrix(NaN,nrow=Nsample,ncol = ncol(x))
+    normaldist=matrix(NaN,nrow=Nsample,ncol = ncol(x))
+    for (i in 1:ncol(x)) {
+      shat[i] <- min(std[i], iqr[i]/faktor, na.rm = TRUE)
+      mhat[i] <- mean(x[, i], trim = 0.1, na.rm = TRUE)
+      nonunimodal[i]=diptest::dip.test(x[, i])$p.value
+      skewed[i]=moments::agostino.test(x[, i])$p.value
+      if(nonunimodal[i]<0.05|skewed[i]<0.05)
+        normaldist[,i] <- rnorm(Nsample, mhat[i], shat[i])
+    }
+    colnames(normaldist)=colnames(Data)
+    DFtemp = reshape2::melt(normaldist)
+    colnames(DFtemp) <- c('ID', 'Variables', 'Values')
+    DFtemp$Variables=as.character(DFtemp$Variables)
+    print(shat)
+    plot=plot+geom_violin(data = DFtemp,mapping = aes_string(x = "Variables", group = "Variables", y = "Values"),colour='black',alpha=0,scale=scale,size=size)+guides(fill=FALSE)
+    #plot=plot+geom_boxplot(width=1,outlier.colour = NA,alpha=0)
+    }
   return(ggplotObj = plot+ggExtra::rotateTextX())
 } 
