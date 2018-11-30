9fcde5c0daab7e4d9a8d4bc6b624c9735b127db3
Mthrun <m.thrun@gmx.net>
2018-11-30 18:07:06 +0100

    bugfix

--- R/PlotMissingValues.R
+++ R/PlotMissingValues.R
@@ -20,7 +20,10 @@ PlotMissingValues=function(Dataframe,WhichMissing=c('NA','NaN','DUMMY','.',' '),
   if(any(WhichMissing=="DUMMY")){
     dummy=c()
     for(i in 1:d){
-      dummy=c(dummy,sum(as.vector(as.matrix(Dataframe[,i]))=="DUMMY",na.rm = T))
+      f1=sum(as.vector(as.matrix(Dataframe[,i]))=="DUMMY",na.rm = T)
+      f2=sum(as.vector(as.matrix(Dataframe[,i]))=="Dummy",na.rm = T)
+      f3=sum(as.vector(as.matrix(Dataframe[,i]))=="dummy",na.rm = T)
+      dummy=c(dummy,f2+f1+f3)
     }
   }
   if(any(WhichMissing==".")){
@@ -38,7 +41,8 @@ PlotMissingValues=function(Dataframe,WhichMissing=c('NA','NaN','DUMMY','.',' '),
 
   nana=nas+nans+dummy+point+blank
   names(nana)=colnames(Dataframe)
-  par(mar = c(5, 18, 2, 2) + 0.2)
+  par(mar = c(5, 18, 1, 1) + 0.2)
+  options(repr.plot.width=4, repr.plot.height=8)
   par(las=2) 
   barplot(nana/ncases*100,horiz = T,...)
   par(def.par)
