316ad262ffa29d23909751b63e36dd1fa6339b05
Mthrun <m.thrun@gmx.net>
2019-11-15 14:51:32 +0100

    plotyl subplot bug umgangen

--- R/DualaxisLinechart.R
+++ R/DualaxisLinechart.R
@@ -1,21 +1,20 @@
-DualaxisLinechart=DualAxisLineChart=function(X,Y1,Y2,xlab='X',y1lab='Y1',y2lab='Y2',main='Dual Axis Line Chart',cols=c('black','blue'),SaveIt=FALSE){
+DualaxisLinechart=DualAxisLineChart=function(X,Y1,Y2,xlab='X',y1lab='Y1',y2lab='Y2',main='Dual Axis Line Chart',cols=c('black','blue'),Overlaying='y',SaveIt=FALSE){
   requireNamespace('plotly')
-
-  
   p <- plotly::plot_ly() 
-    p <- plotly::add_lines(p,x = ~X, y = ~Y1, name = y1lab, line = list(color=cols[1])) 
+    p <- plotly::add_lines(p,x = ~X, y = ~Y1, name = y1lab, yaxis = "y", line = list(color=cols[1])) 
     p <-  plotly::add_lines(p,x = ~X, y = ~Y2, name = y2lab, yaxis = "y2", line = list(color=cols[2])) 
     p <-  plotly::layout(p,
       title = main, 
       yaxis2 = list(
-        overlaying = "y",
         side = "right",
         title = y2lab,
+        overlaying = Overlaying,
         showgrid=FALSE
       ),
       xaxis = list(title=xlab,
                    showgrid=FALSE),
       yaxis= list(
+        side = "left",
         title=y1lab,
         showgrid=FALSE
         )
